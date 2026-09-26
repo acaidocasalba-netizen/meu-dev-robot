28e7a0b9a736fd2f91fbbc0be69ddbb5a4717a8c
acaidocasalba-netizen <acaidocasal.ba@gmail.com>
2026-09-25 21:12:06 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ async function getRobot() {
       if (navigator.gpu) {
         robot = await pipeline(
           "text-generation",
-        "onnx-community/SmolLM2-135M-Instruct-ONNX-MHA",
+        "onnx-community/Qwen2.5-Coder-1.5B-Instruct",
           {
             dtype: "q4",
             device: "webgpu",
